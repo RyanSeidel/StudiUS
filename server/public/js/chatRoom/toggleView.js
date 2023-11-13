6914c943382f4d406655f434c8ba3bc04c681d36
NuttyRyan <76074138+woahitsryan@users.noreply.github.com>
2023-11-13 15:23:52 -0600

    chat is fk atm

--- server/public/js/chatRoom/toggleView.js
+++ server/public/js/chatRoom/toggleView.js
@@ -1,19 +1,16 @@
 function toggleView(view) {
   const chatBox = document.getElementById('chat-box');
-  const participantsList = document.getElementById('participants-list');
-  const msgBottom = document.getElementsByClassName('msg-bottom')[0]; // Get the msg-bottom div
+  const msgBottom = document.getElementsByClassName('msg-bottom')[0];
   const chatBtn = document.getElementById('chatBtn');
   const participantsBtn = document.getElementById('participantsBtn');
 
   if (view === 'chat') {
       chatBox.style.display = 'block'; // Show chat box
-      participantsList.style.display = 'none'; // Hide participants list
-      msgBottom.style.display = 'block'; // Show msg-bottom div
+      msgBottom.style.display = 'flex'; // Show msg-bottom div
       chatBtn.classList.add('active-btn');
       participantsBtn.classList.remove('active-btn');
-  } else {
+  } else if (view === 'participants') {
       chatBox.style.display = 'none'; // Hide chat box
-      participantsList.style.display = 'block'; // Show participants list
       msgBottom.style.display = 'none'; // Hide msg-bottom div
       chatBtn.classList.remove('active-btn');
       participantsBtn.classList.add('active-btn');
