a50b113d4364bcfab350f36fc8d8599e7fbd48ec
NuttyRyan <76074138+woahitsryan@users.noreply.github.com>
2023-11-15 00:11:01 -0600

    added names to create room
    - now adds name instead of ids

--- server/public/js/home/fetchRooms.js
+++ server/public/js/home/fetchRooms.js
@@ -1,40 +1,41 @@
 function fetchRooms() {
-  fetch('/get-rooms')
-      .then(response => {
-          if (!response.ok) {
-              throw new Error('Network response was not ok');
-          }
-          return response.json();
-      })
-      .then(rooms => {
-          const roomsListDiv = document.getElementById('rooms-list');
-          rooms.forEach(room => {
-              const roomDiv = document.createElement('div');
-              roomDiv.className = 'room';
-              roomDiv.style.cursor = 'pointer';
-              roomDiv.style.margin = '10px 0';  // Margin for spacing between each room
+    fetch('/get-rooms')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(rooms => {
+            const roomsListDiv = document.getElementById('rooms-list');
+            rooms.forEach(room => {
+                const roomDiv = document.createElement('div');
+                roomDiv.className = 'room';
+                roomDiv.style.cursor = 'pointer';
+                roomDiv.style.margin = '10px 0';  // Margin for spacing between each room
 
-              const roomName = document.createElement('div');
-              roomName.textContent = `Group Name: ${room.name}`;
-              roomName.style.marginBottom = '5px';  // Margin for spacing between room name and member count
+                const roomName = document.createElement('div');
+                roomName.textContent = `Group Name: ${room.name}`;
+                roomName.style.marginBottom = '5px';  // Margin for spacing between room name and member count
 
-              const roomMembers = document.createElement('div');
-              roomMembers.textContent = `${room.userIds.length} members`;
-              roomMembers.style.fontSize = '12px';
+                const roomMembers = document.createElement('div');
+                // Use the length of the userNames array to display the number of members
+                roomMembers.textContent = `${room.userNames.length} members`;
+                roomMembers.style.fontSize = '12px';
 
-              roomDiv.appendChild(roomName);
-              roomDiv.appendChild(roomMembers);
+                roomDiv.appendChild(roomName);
+                roomDiv.appendChild(roomMembers);
 
-              roomDiv.addEventListener('click', () => {
-                  window.location.href = `/chatroom?roomId=${room._id}`;
-              });
+                roomDiv.addEventListener('click', () => {
+                    window.location.href = `/chatroom?roomId=${room._id}`;
+                });
 
-              roomsListDiv.appendChild(roomDiv);
-          });
-      })
-      .catch(error => {
-          console.error("Error fetching rooms:", error);
-      });
+                roomsListDiv.appendChild(roomDiv);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching rooms:", error);
+        });
 }
 
 fetchRooms();
@@ -54,16 +55,47 @@ initCreateRoomBtn.addEventListener('click', () => {
         return;
     }
 
-    // Show the names of the selected users in the modal
-    selectedUsersDisplay.innerHTML = ""; // Clear previous users
-    selectedUsers.forEach(userId => {
-        const userDiv = document.createElement('div');
-        userDiv.textContent = userId; // For now, using userId as display. Modify this as needed.
-        selectedUsersDisplay.appendChild(userDiv);
+    // Fetch user names based on selected user IDs
+    fetch('/get-user-names', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ userIds: selectedUsers })
+    })
+    .then(response => response.json())
+    .then(userNames => {
+        selectedUsersDisplay.innerHTML = ""; // Clear previous users
+
+        userNames.forEach((userName, index) => {
+            const userDiv = document.createElement('div');
+            userDiv.textContent = userName;
+            userDiv.style.display = 'flex';
+            userDiv.style.alignItems = 'center';
+            userDiv.style.justifyContent = 'space-between';
+
+            // Create the remove button
+            const removeBtn = document.createElement('button');
+            removeBtn.textContent = 'X';
+            removeBtn.style.marginLeft = '10px';
+
+            // Event listener for the remove button
+            removeBtn.addEventListener('click', () => {
+                selectedUsers.splice(index, 1); // Remove the user from the array
+                userDiv.remove(); // Remove the user div from the display
+            });
+
+            userDiv.appendChild(removeBtn);
+            selectedUsersDisplay.appendChild(userDiv);
+        });
+    })
+    .catch(error => {
+        console.error("Error fetching user names:", error);
     });
 
     createRoomModal.style.display = "block"; // Show the modal
 });
+
+
+
 
 closeCreateRoomModalBtn.onclick = () => createRoomModal.style.display = "none";
 
