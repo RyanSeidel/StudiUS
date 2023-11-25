ec461bd521722320724199ffd26b449727a8022d
lgp32 <lgonzales29@islander.tamucc.edu>
2023-11-25 17:34:52 -0600

    todolist
    i accidentally deleted code for the todolist. but i fixed it here we are : )

--- server/public/js/home/todolist.js
+++ server/public/js/home/todolist.js
@@ -1,18 +1,12 @@
 /*
    TASK LIST JAVASCRIPT
-
    This JavaScript code is responsible for managing a simple task list on a web page. It allows users to add tasks, view them, and delete them. The code uses the DOM (Document Object Model) to interact with HTML elements.
-
    Key components and functionalities:
-
    - Event listener is set up to trigger actions when the DOM content is loaded.
    
    - The code selects relevant HTML elements using `getElementById`.
-
    - The `addTask` function is used to add a new task to the task list. It first checks if the input is empty and displays an alert if so. Then, it creates a task item with a task text and a delete button. Clicking the delete button removes the task item.
-
    - Event listeners are added to the "Add" button to call the `addTask` function when clicked.
-
    Note: To use this code, you should have corresponding HTML elements with matching IDs in your web page. You may also need to include appropriate CSS styles for the visual presentation of the task list.
 */
 // Define closeModal and addTaskModal functions in the global scope
@@ -21,95 +15,55 @@ document.addEventListener("DOMContentLoaded", function () {
     const openTaskModalButton = document.getElementById("openTaskModal");
     const closeModalButton = document.querySelector(".close");
     const taskInputModal = document.getElementById("taskInputModal");
+    const dateInputModal = document.getElementById("dateInputModal");
     const tasksContainer = document.getElementById("tasks");
-
     function openModal() {
         taskModal.style.display = "block";
     }
-
     function closeModal() {
         taskModal.style.display = "none";
         taskInputModal.value = "";
+        dateInputModal.value = ""; // Reset the date input
     }
-
     function addTaskModal() {
         const taskText = taskInputModal.value.trim();
+        const taskDate = dateInputModal.value;
         
         if (taskText === "") {
             alert("Please enter a task!");
             return;
         }
-
         const taskItem = document.createElement("div");
         taskItem.classList.add("task-item");
-
         const taskTextElement = document.createElement("div");
         taskTextElement.classList.add("task-text");
         taskTextElement.textContent = taskText;
-
+        // Optionally, add the date if provided
+        if (taskDate) {
+            const taskDateElement = document.createElement("div");
+            taskDateElement.classList.add("task-date");
+            taskDateElement.textContent = `Due Date: ${taskDate}`;
+            taskItem.appendChild(taskDateElement);
+        }
         const deleteButton = document.createElement("div");
         deleteButton.classList.add("delete-task");
         deleteButton.textContent = "Delete";
-
         deleteButton.addEventListener("click", function () {
             taskItem.remove();
         });
-
         taskItem.appendChild(taskTextElement);
         taskItem.appendChild(deleteButton);
         tasksContainer.appendChild(taskItem);
-
         closeModal();
     }
-
     openTaskModalButton.addEventListener("click", openModal);
     closeModalButton.addEventListener("click", closeModal);
+    // Attach the event listener for adding a task to the button in the modal
+    const addTaskButton = document.querySelector(".modal .addTask");
+    addTaskButton.addEventListener("click", addTaskModal);
     window.addEventListener("click", function (event) {
         if (event.target === taskModal) {
             closeModal();
         }
-    });
-});
-//dark mode
-document.addEventListener("DOMContentLoaded", function () {
-    const body = document.body;
-    const darkModeToggle = document.getElementById("darkModeToggle");
-
-    // Check user preference from local storage
-    const isDarkMode = localStorage.getItem("darkMode") === "enabled";
-
-    // Set initial theme based on user preference
-    if (isDarkMode) {
-        body.classList.add("dark-mode");
-    } else {
-        body.classList.remove("dark-mode");
-    }
-
-    // Event listener for dark mode toggle button
-    darkModeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-        // Update user preference in local storage
-        const newMode = body.classList.contains("dark-mode") ? "enabled" : "disabled";
-        localStorage.setItem("darkMode", newMode);
-    });
-});
-document.addEventListener("DOMContentLoaded", function () {
-    const body = document.body;
-    const darkModeToggle = document.getElementById("darkModeToggle");
-
-    // Check user preference from local storage
-    const isDarkMode = localStorage.getItem("darkMode") === "enabled";
-
-    // Set initial theme based on user preference
-    if (isDarkMode) {
-        body.classList.add("dark-mode");
-    }
-
-    // Event listener for dark mode toggle button
-    darkModeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-        // Update user preference in local storage
-        const newMode = body.classList.contains("dark-mode") ? "enabled" : "disabled";
-        localStorage.setItem("darkMode", newMode);
     });
 });
